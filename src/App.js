c10e38da93c9430ea4f92c436a320ba752567e0a
will <williamwalker753@gmail.com>
2022-07-31 03:16:47 +0000

    add pages workflow

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import { h } from 'preact';
 import { Router } from 'preact-router';
+import baseroute from './baseroute';
 
-import Header from '/components/header';
-import Home from '/routes/home';
-import About from '/routes/about';
+import Header from './components/header';
+import Home from './routes/home';
+import About from './routes/about';
 
 const App = () => (
 	<div id="app">
 		<Header />
 		<Router>
-			<Home path="/" />
-			<About path="/about" />
+			<Home path={`${baseroute}/`} />
+			<About path={`${baseroute}/about`} />
 		</Router>
 	</div>
 )
