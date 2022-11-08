f9cf87e1298ef82a98b9cd82fda9716be6b22149
will <williamwalker753@gmail.com>
2022-11-08 04:28:45 +0000

    Fix window undefined build error

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import About from './routes/about';
 const App = () => {
 	// Dynamic routing, enables users to go directly to non root url paths
 	// See 404.html for details
-	if (window.location.hash.length > 0) {
+	if (window?.location?.hash?.length > 0) {
 		window.location.href = window.location.hash.replace('#!', '');
 	}
 
